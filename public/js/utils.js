8c57572578a6bc910d7d38ae9ebfbe2daf9f387f
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2013-02-19 12:20:35 -0800

    Javascript utilities.

---
+++ public/js/utils.js
@@ -0,0 +1,17 @@
+window.utils = {
+    loadTemplate: function(views, callback) {
+	var deferreds = [];
+
+	$.each(views, function(index, view) {
+	    if (window[view]) {
+		deferreds.push($.get('templates/' + view + '.handlebars', function (data) {
+		    window[view].prototype.template = Handlebars.compile(data);
+		}))
+	    } else {
+		alert('View not found: ' + view);
+	    }
+	});
+
+	$.when.apply(null, deferreds).done(callback);
+    }
+}
