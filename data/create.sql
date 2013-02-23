9b94f708f6f04f2bbd9918f5c740b0af9153f595
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2013-02-22 20:23:11 -0800

    Modifed create sql file.

--- data/create.sql
+++ data/create.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS "categories" ("id"   SERIAL , "name" VARCHAR(255), "c
 CREATE TABLE IF NOT EXISTS "guides" ("id"   SERIAL , "name" VARCHAR(255) NOT NULL, "city" VARCHAR(255) NOT NULL, "createdAt" TIMESTAMP NOT NULL, "updatedAt" TIMESTAMP NOT NULL, PRIMARY KEY ("id"));
 CREATE TABLE IF NOT EXISTS "locations" ("id"   SERIAL , "address" VARCHAR(255), "city" VARCHAR(255), "country" VARCHAR(255), "lat" FLOAT, "lng" FLOAT, "postalCode" INTEGER, "state" VARCHAR(255), "createdAt" TIMESTAMP NOT NULL, "updatedAt" TIMESTAMP NOT NULL, "placeId" INTEGER, PRIMARY KEY ("id"));
 CREATE TABLE IF NOT EXISTS "places" ("id"   SERIAL , "name" VARCHAR(255), "createdAt" TIMESTAMP NOT NULL, "updatedAt" TIMESTAMP NOT NULL, "guideId" INTEGER, "locationId" INTEGER, PRIMARY KEY ("id"));
-CREATE TABLE IF NOT EXISTS "guidesplaces" ("placeId" INTEGER , "guideId" INTEGER , "createdAt" TIMESTAMP NOT NULL, "updatedAt" TIMESTAMP NOT NULL, PRIMARY KEY ("placeId","guideId"));CREATE TABLE locations;
+CREATE TABLE IF NOT EXISTS "guidesplaces" ("placeId" INTEGER , "guideId" INTEGER , "createdAt" TIMESTAMP NOT NULL, "updatedAt" TIMESTAMP NOT NULL, PRIMARY KEY ("placeId","guideId"));
